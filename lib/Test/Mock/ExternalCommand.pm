90c22c20f9549ecb3f63d5db6d557eafc1a878de
Takuya Tsuchida <tsucchi@cpan.org>
2013-02-05 08:18:59 +0000

    Import of TSUCCHI/Test-Mock-ExternalCommand-0.03 from CPAN.
    gitpan-cpan-distribution: Test-Mock-ExternalCommand
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TSUCCHI/Test-Mock-ExternalCommand-0.03.tar.gz
    gitpan-cpan-author:       TSUCCHI
    gitpan-cpan-maturity:     released

--- lib/Test/Mock/ExternalCommand.pm
+++ lib/Test/Mock/ExternalCommand.pm
@@ -5,8 +5,8 @@ use Config;
 use Carp;
 use Variable::Expand::AnyLevel qw(expand_variable);
 
-use 5.008;
-our $VERSION = '0.02';
+use 5.010;
+our $VERSION = '0.03';
 
 my $command_registry = {};
 my $command_history = {};
